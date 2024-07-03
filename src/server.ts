a23e394e9948fb35bcbe2df6244bfca79e51f856
Derrick Muturi <wawerumuturi57@gmail.com>
2024-07-03 14:56:01 +0300

    build

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
+import nextBuild from "next/dist/build";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -35,6 +36,16 @@ const start = async () => {
       createContext,
     })
   );
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info("Next js is building for production ");
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+      process.exit();
+    });
+    return;
+  }
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
