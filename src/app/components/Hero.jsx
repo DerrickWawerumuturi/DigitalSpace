a01ef95e7b7cd77e1a24e8ec03ccca132b1e4e6d
Derrick Muturi <wawerumuturi57@gmail.com>
2024-07-04 11:25:57 +0300

    final changes

--- src/app/components/Hero.jsx
+++
@@ -1,7 +0,0 @@
-const Hero = () => {
-  <div className="flex sm:flex-row gap-4">
-    <div>{/* DISPLAY PRODUCTS */}</div>
-  </div>;
-};
-
-export default Hero;
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
 import nextBuild from "next/dist/build";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -37,6 +39,21 @@ const start = async () => {
     })
   );
 
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+    const pareUrl = parse(req.url, true);
+
+    return nextApp.render(req, res, "/cart", pareUrl.query);
+  });
+
+  app.use("/cart", cartRouter);
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info("Next js is building for production ");
