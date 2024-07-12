04d75e4fe9b1e02e36be91df461e81ab78b791b8
Derrick Muturi <wawerumuturi57@gmail.com>
2024-07-12 14:22:27 +0300

    navbar

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -68,7 +68,7 @@ const page = async ({ params }: PageProps) => {
     return (
         <MaxWidthWrapper className='bg-white'>
             <div className='bg-white'>
-                <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-10 lg:px-8'>
+                <div className='mx-auto max-w-2xl px-4 lg:mr-10 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-10 lg:px-8'>
                     {/* Product Details */}
                     <div className='lg:max-w-lg lg:self-end'>
                         <ol className='flex items-center space-x-2'>
